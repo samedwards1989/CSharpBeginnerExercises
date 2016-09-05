4401b2ad129610e5fdcf009961af699d28f7739d
Sam Edwards <uk_sam2003@hotmail.com>
2016-09-05 08:12:11 +0100

    Exercise 1
    Write a program and ask the user to enter a number.The number should be
    between 1 to 10.
    If the user enters a valid number, display "Valid" on the
    console.Otherwise, display "Invalid".

--- CSharpBeginnerExercises/ControlFlow/Program.cs
+++ CSharpBeginnerExercises/ControlFlow/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ControlFlow
+﻿namespace ControlFlow
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Exercise 1
+            // Write a program and ask the user to enter a number.The number should be between 1 to 10.
+            // If the user enters a valid number, display "Valid" on the console.Otherwise, display "Invalid".
+            // (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+
+            //            Console.WriteLine("Enter a number between 1 and 10");
+            //            var input = Console.ReadLine();
+            //            try
+            //            {
+            //                var number = Convert.ToInt32(input);
+            //
+            //                if (number < 1 || number > 10)
+            //                    Console.WriteLine("Invalid");
+            //                else
+            //                    Console.WriteLine("Valid");
+            //            }
+            //            catch (Exception e)
+            //            {
+            //
+            //                Console.WriteLine(e);
+            //            }
+
+            // Exercise 2
+            // Write a program which takes two numbers from the console and displays the maximum of the two
         }
     }
 }
